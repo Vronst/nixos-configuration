65c62d56c52676213bed2b62cf741e4c1fb0507c
Adam Sarga <vronst@gmail.com>
2026-04-05 22:07:11 +0200

    no progrs with homemanager

--- modules/features/niri/niri_conf.nix
+++ modules/features/niri/niri_conf.nix
@@ -3,7 +3,7 @@
 {
   flake.nixosModules.conf_niri = { pkgs, ... }: {
 
-    home-manager.users.${config.username} = {
+    home-manager.users.vronst = {
       xdg.configFile."niri/config.kdl".source = ./extra-config.kdl;
     };
   };
